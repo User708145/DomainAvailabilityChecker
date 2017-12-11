e1fd7df88eff76a097d7f96e5a75323191f53250
Tobias Tjørnelund <tobi.mig@gmail.com>
2017-12-11 10:56:49 +0100

    Fixed minor bugs

--- main.go
+++ main.go
@@ -1,86 +1,93 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "net"
-  "sync"
-  "github.com/likexian/whois-go"
-  "github.com/likexian/whois-parser-go"
-  "github.com/fatih/color"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/likexian/whois-go"
+	"github.com/likexian/whois-parser-go"
+	"net"
+	"os"
+	"sync"
 )
 
 const (
-  loading = iota
-  available
-  unavailable
+	loading = iota
+	available
+	unavailable
 )
 
 func main() {
-  if len(os.Args) < 2 {
-    fmt.Println("Please enter a domain name")
-    return
-  }
+	if len(os.Args) < 2 {
+		fmt.Println("Please enter a domain name")
+		return
+	}
 
-  input := os.Args[1]
-  tlds := []string{"com", "net", "dk", "org", "xxx", "io", "co.uk"}
+	input := os.Args[1]
+	tlds := []string{"com", "net", "dk", "org", "xxx", "io", "co.uk"}
 
-  var state sync.Map
-  for _, tld := range tlds {
-    state.Store(tld, loading)
-  }
+	var state sync.Map
+	for _, tld := range tlds {
+		state.Store(tld, loading)
+	}
 
-  hasPrintedState := false
-  printState := func() {
-    if hasPrintedState {
-      fmt.Printf("\033[%vF", len(tlds))
-    } else {
-      hasPrintedState = true
-    }
+	hasPrintedState := false
 
-    for _, tld := range tlds {
-      name := input + "." + tld
-      line := name + ": "
-      value, _ := state.Load(tld)
-      switch value {
-      case available: line += color.GreenString("Available")
-      case unavailable: line += color.RedString("Unavailable")
-      }
-      fmt.Println(line)
-    }
-  }
+	mutex := &sync.Mutex{}
 
-  var wg sync.WaitGroup
-  wg.Add(len(tlds) * 2)
+	printState := func() {
+		mutex.Lock()
+		if hasPrintedState {
+			fmt.Printf("\033[%vF", len(tlds))
+		} else {
+			hasPrintedState = true
+		}
 
-  for i := range tlds {
-    tld := tlds[i]
-    name := input + "." + tld
+		for _, tld := range tlds {
+			name := input + "." + tld
+			line := name + ": "
+			value, _ := state.Load(tld)
+			switch value {
+			case available:
+				line += color.GreenString("Available")
+			case unavailable:
+				line += color.RedString("Unavailable")
+			}
+			fmt.Println(line)
+		}
+		mutex.Unlock()
+	}
 
-    go func() {
-      defer wg.Done()
-      _, err := net.LookupHost(name)
-      if err == nil {
-        state.Store(tld, unavailable)
-        printState()
-      }
-    }()
+	var wg sync.WaitGroup
+	wg.Add(len(tlds) * 2)
 
-    go func() {
-      defer wg.Done()
-      rawResult, _ := whois.Whois(name)
-      result, err := whois_parser.Parser(rawResult)
-      if err == nil {
-        if result.Registrar.DomainStatus != "" {
-          state.Store(tld, unavailable)
-          printState()
-        } else {
-          state.Store(tld, available)
-          printState()
-        }
-      }
-    }()
-  }
+	for i := range tlds {
+		tld := tlds[i]
+		name := input + "." + tld
 
-  wg.Wait()
+		go func() {
+			defer wg.Done()
+			_, err := net.LookupHost(name)
+			if err == nil {
+				state.Store(tld, unavailable)
+				printState()
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			rawResult, _ := whois.Whois(name)
+			result, err := whois_parser.Parser(rawResult)
+			if err == nil {
+				if result.Registrar.DomainStatus != "" {
+					state.Store(tld, unavailable)
+					printState()
+				} else {
+					state.Store(tld, available)
+					printState()
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
 }
